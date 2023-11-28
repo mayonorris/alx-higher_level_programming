baf153486bbe5c671ee47fe60c9abf80eda09218
Mayo <kadanganorris@gmail.com>
2023-11-28 22:26:11 +0100

    conver to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        # Check if the character is a lowercase letter
+def uppercase(s):
+    for char in s:
         if 97 <= ord(char) <= 122:
             equival = 32
         else:
             equival = 0
-        print("{:c}".format(ord(char)-equival, end = "")
+        print("{:c}".format(ord(char)-equival), end="")
     print()
