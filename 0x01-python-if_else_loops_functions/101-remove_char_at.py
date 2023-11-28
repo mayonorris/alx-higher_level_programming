c1eef1c35874842ebec78bbc943fa5546d68e39e
Mayo <kadanganorris@gmail.com>
2023-11-29 00:39:29 +0100

    creates a copy of a string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str_copy = str[:n] + str[n+1:]
-    return (str_copy)
-
+    if n < 0:
+        return(str)
+    else:
+        return (str[:n] + str[n+1:])
