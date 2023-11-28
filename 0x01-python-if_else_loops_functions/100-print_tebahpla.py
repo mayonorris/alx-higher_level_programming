d64442921481562a378d9e3bca6ec6579b720b39
Mayo <kadanganorris@gmail.com>
2023-11-29 00:15:59 +0100

    talphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print(f"{chr(i)}{chr(i - 32)}", end="")
+for i in range(122, 96, -2):
+    k = i-1
+    print("{:c}{:c}".format(i, k-32), end="")
