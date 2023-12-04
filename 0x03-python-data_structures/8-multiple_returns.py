7c117ceaff7635ed162850f223042e33288c6291
Mayo <kadanganorris@gmail.com>
2023-12-05 00:20:01 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+     l = len(sentence)
     if sentence == '':
-        return (None)
-    l = len(sentence)
+        return(l, None)
     first_char = sentence[0]
     return (l, first_char)
