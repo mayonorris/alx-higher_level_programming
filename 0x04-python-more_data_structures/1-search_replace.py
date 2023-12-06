05db29e68e820862874d32619e8509a4101ae5e0
Mayo <kadanganorris@gmail.com>
2023-12-07 00:18:52 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
   my_list = my_list.copy()
+
   for idx, val in enumerate(my_list):
     if val == search:
       my_list[idx] = replace
