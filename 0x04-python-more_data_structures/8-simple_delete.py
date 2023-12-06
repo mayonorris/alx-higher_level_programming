0082313913b9b3be6c1245ac0844c1452b55a2ae
Mayo <kadanganorris@gmail.com>
2023-12-07 00:16:10 +0100

    more data structures

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/python3
+def simple_delete(a_dictionary, key=""):
+  a_dictionary = a_dictionary.copy()
+  if key not in list(a_dictionary):
+    return (a_dictionary)
+  del(a_dictionary)[key]
+  return (a_dictionary)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+  for key  in list(a_dictionary):
+    a_dictionary[key] = 2*a_dictionary[key]
+  return (a_dictionary)
