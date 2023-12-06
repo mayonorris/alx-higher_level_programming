8b2000cccc0af8aeef46da4927cf793f0a8f18ae
Mayo <kadanganorris@gmail.com>
2023-12-06 23:47:37 +0100

    compute squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_cp = matrix.copy()
-    sq_matrix = ([list(map(lambda elt: elt**2, row)) for row in matrix_cp])
+    matrix = matrix.copy()
+    sq_matrix = [list(map(lambda elt: elt**2, row)) for row in matrix]
   return (sq_matrix)
