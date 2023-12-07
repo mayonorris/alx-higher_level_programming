b47299760eabbd2568f8bd1e79a17ce0fb69853f
Mayo <kadanganorris@gmail.com>
2023-12-07 01:07:13 +0100

    simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary = a_dictionary.copy()
-
     if key not in list(a_dictionary):
         return (a_dictionary)
     del(a_dictionary)[key]
