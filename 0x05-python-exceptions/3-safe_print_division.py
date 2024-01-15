d84a38567f8c0b1d781afd7febe8c76d9172875f
Mayo <kadanganorris@gmail.com>
2024-01-15 05:48:07 +0100

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,17 +2,11 @@
 
 def safe_print_division(a, b):
  """Divides two integers and prints the result within a finally block."""
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Inside result: {}".format(None))
-        return (None)
-    except Exception:
-        print("Inside result: {}".format(None))
-        return (None)
-    else:
-        print("Inside result: {}".format(result))
-        return (result)
-    finally:
-        print("Inside result: {}".format(result))
 
+    try:
+        res = a / b
+    except (TypeError, ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return (res)
