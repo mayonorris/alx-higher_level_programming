3f95351a621600566d25a971b1f58b0e715393ad
Mayo <kadanganorris@gmail.com>
2024-01-15 05:41:29 +0100

    safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+ """Divides two integers and prints the result within a finally block."""
+
+ result = None
+
+ try:
+   result = a / b
+ except ZeroDivisionError:
+   print("Error: Cannot divide by zero!")
+ finally:
+   print("Inside result: {}".format(result))
+
+ return result
+
