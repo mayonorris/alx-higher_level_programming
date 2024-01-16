a9e18ac29ad2f8451185064a7df216ae759cb249
Mayo <kadanganorris@gmail.com>
2024-01-16 02:42:30 +0100

    empty square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module - Square
+This module contains an empty class Square.
+"""
+
+
+class Square:
+    """
+    Class - Square
+    An empty class representing a square.
+    """
+    pass
+
