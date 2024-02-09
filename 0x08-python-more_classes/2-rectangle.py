f354b8b0fa83f38badca68cddad7833ab1210ab2
Mayo <kadanganorris@gmail.com>
2024-02-09 15:05:08 +0100

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         """Method to calculate the rectangle perimeter"""
         if (self.__width is 0 or self.__height is 0):
             return (0)
-        return (2 * (self.__width + self.__height))
+        return (self.__width *2 + self.__height * 2)
