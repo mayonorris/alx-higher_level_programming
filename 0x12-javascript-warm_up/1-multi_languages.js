a655c004f4647d40e1488f43f0065a9acdcfe66a
Mayo <kadanganorris@gmail.com>
2024-03-14 01:29:55 +0100

    print three lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// Print the first line: "C is fun"
-console.log("C is fun");
-// Print the second line: "Python is cool"
-console.log("Python is cool");
-// Print the third line: "JavaScript is amazing"
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
