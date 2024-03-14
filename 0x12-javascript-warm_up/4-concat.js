07460b1248afa2426afacc367a898d4822289968
Mayo <kadanganorris@gmail.com>
2024-03-14 01:52:10 +0100

    create sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,5 +4,5 @@ const secondArg = process.argv[3];
 if (firstArg !== undefined && secondArg !== undefined) {
     console.log(`${firstArg} is ${secondArg}`);
 } else {
-    console.log("undefined is undefined");
+    console.log('undefined is undefined');
 }
