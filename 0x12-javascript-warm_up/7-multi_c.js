a3398c3a77d9e1fd793306505b71f608e9d91f24
Mayo <kadanganorris@gmail.com>
2024-03-14 02:37:42 +0100

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (!isNaN(x) && x > 0) {
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
 } else {
-    console.log('Missing number of occurrences');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
