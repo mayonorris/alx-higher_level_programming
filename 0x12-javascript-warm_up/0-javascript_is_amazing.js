4518659dffac382d136b99a585ee943f929bf6f5
Mayo <kadanganorris@gmail.com>
2024-03-14 01:22:24 +0100

    print using console.log()

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-
-// Define a constant variable called myVar with the value "JavaScript is amazing"
 const myVar = "JavaScript is amazing";
-
-// Print the value of myVar using console.log(...)
 console.log(myVar);
-
