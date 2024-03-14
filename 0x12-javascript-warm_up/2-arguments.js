51e2928ec7f861bcfda1eab2358fecc895ce0aa8
Mayo <kadanganorris@gmail.com>
2024-03-14 01:39:22 +0100

    print message depending on the number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const numOfArgs = process.argv.length - 2;
 if (numOfArgs === 0) {
-    console.log("No argument");
+    console.log('No argument');
 } else if (numOfArgs === 1) {
-    console.log("Argument found");
+    console.log('Argument found');
 } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
