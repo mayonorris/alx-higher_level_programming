3c4918520ba951aa06b03997e4fb565e85c01a3a
Mayo <kadanganorris@gmail.com>
2024-03-14 01:53:29 +0100

    create sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-const secondArg = process.argv[3];
-if (firstArg !== undefined && secondArg !== undefined) {
-    console.log(`${firstArg} is ${secondArg}`);
-} else {
-    console.log('undefined is undefined');
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
