0286e09e9371b05a73b0a6e181646035f3122d1b
Mayo <kadanganorris@gmail.com>
2024-03-16 06:20:20 +0100

    number of occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-module.exports.nbOccurences = function(list, searchElement) {
+module.exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const element of list) {
-    if (element === searchElement) {
-      count++;
-    }
+    if (element === searchElement) count++;
   }
   return (count);
 };
