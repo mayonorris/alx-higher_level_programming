6d518a2190516c3c39cdf961178c62a6de6fbb9e
Mayo <kadanganorris@gmail.com>
2024-03-18 02:16:24 +0100

    displays all values in the states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,20 +7,36 @@ where name matches the provided argument, using safe parameterized queries.
 import MySQLdb
 import sys
 
+def filter_states(username, password, database, state_name):
+    """Displays all values in the states table where name matches the provided argument."""
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        cursor.close()
+        db.close()
+
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
+    filter_states(username, password, database, state_name)
