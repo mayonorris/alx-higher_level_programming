ffcdbfe6c4374343c551e928e19c8757c5f5688b
Mayo <kadanganorris@gmail.com>
2024-03-28 01:05:45 +0100

    display body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response
-[[ $(curl -s -o /dev/null -w "%{http_code}" "$1") -eq 200 ]] && curl -s "$1"
+curl -sL "$1"
