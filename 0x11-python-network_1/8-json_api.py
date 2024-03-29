0fd74a1c073fc3666ce3e2bb41d81b116b846b72
Mayo <kadanganorris@gmail.com>
2024-03-29 01:57:00 +0100

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter, sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter, and displays
+the response according to the specified format.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
