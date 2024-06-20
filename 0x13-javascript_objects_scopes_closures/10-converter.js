654c0a9c009fb5a782733bb1c60e506a78c700e1
Mayo <kadanganorris@gmail.com>
2024-06-20 03:48:22 +0100

    convert from base 10 to another base

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return function (number) {
+    if (number === 0) {
+      return ('');
+    } else {
+      return (exports.converter(base)(Math.floor(number / base)) + String(number % base));
+    }
+  };
+};
